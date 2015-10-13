78fde3e4643aa6afbb78818d82725010ea3a5221
George Toothman <george.toothman@flatironschool.com>
2015-10-13 14:35:40 -0400

    attempt solution

--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,25 @@
 # Implement your object-oriented solution here!
+require 'pry'
+
+class Prime
+  attr_reader :index_num_array
+  def initialize
+    @index_num_array = [*1..10001]
+    self.find_prime_num(index_num_array)
+  end
+
+  def find_prime_num(index_num_array)
+    new_array = Array.new
+    for i in 1...index_num_array.length
+      index = 0
+      if (i % index_num_array[index] != 0) && (i / i == 1) == true
+        new_array << i
+        index += 1
+      else
+        nil
+      end
+    end
+    binding.pry
+  end
+  test = Prime.new
+end
